67df25a4fcc0ac4fca17482af2e803d3b002151e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-05-09 06:35:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("AI司法書士 Assistant")
+st.write("任意後見チャットボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
