fec61d94f01024184b9d3876f5d8a1cc2141c2fb
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-08 06:29:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -88,38 +88,6 @@ options = st.multiselect(
 
 
 
-if 'unique_id' not in st.session_state:
-  st.session_state["unique_id"] = []
-
-col5, col6 = st.columns(2)
-
-with col5:
-  if st.button("保存", key=6):
-    st.session_state["unique_id"].append(uuid.uuid1())
-
-with col6:
-  if st.button("削除", key=7):
-    st.session_state["unique_id"].pop(-1)
-    
-for unique_id in st.session_state["unique_id"]:
-  
-  with st.container():
-    col7, col8 = st.columns(2)
-
-    with col7:
-      slider_value = st.slider(
-        "数値",
-        min_value=0,
-        max_value=14,
-        value=0,
-        key=unique_id
-      )
-    with col8:
-      st.write("")
-      st.write("")
-      st.write(slider_value)
-    
-
 
 def main():
     # 東京のランダムな経度・緯度を生成する
@@ -132,10 +100,12 @@ def main():
     st.map(map_data)
 
     
-if st.session_state["messages"]:
-    messages = st.session_state["messages"]
+
     
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
     
 if __name__ == '__main__':
     main()
