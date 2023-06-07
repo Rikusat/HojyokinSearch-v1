c0aed62dc5311ea7c16d7ddee8232d5bfd4c8995
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-08 05:39:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -81,6 +81,42 @@ options = st.multiselect(
     default=['個人事業主', '子ども家庭'] # デフォルトの設定
 )
 
+
+import uuid
+
+if 'unique_id' not in st.session_state:
+  st.session_state["unique_id"] = []
+
+col5, col6 = st.columns(2)
+
+with col5:
+  if st.button("追加", key=6):
+    st.session_state["unique_id"].append(uuid.uuid1())
+
+with col6:
+  if st.button("削除", key=7):
+    st.session_state["unique_id"].pop(-1)
+    
+for unique_id in st.session_state["unique_id"]:
+  
+  with st.container():
+    col7, col8 = st.columns(2)
+
+    with col7:
+      slider_value = st.slider(
+        "数値",
+        min_value=0,
+        max_value=14,
+        value=0,
+        key=unique_id
+      )
+    with col8:
+      st.write("")
+      st.write("")
+      st.write(slider_value)
+    
+    
+
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
 
