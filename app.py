0702510481d387fa7d8043280b4a2c40c9861dc2
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-17 17:42:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("""
     <style>
     @keyframes robot {
         0% { transform: translateY(0px); }
-        50% { transform: translateY(-150px); }
+        50% { transform: translateY(-120px); }
         100% { transform: translateY(0px); }
     }
     </style>
