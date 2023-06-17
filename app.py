aee389af88777b0d1e84246d0b689e05738ffc81
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-17 13:23:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # Page setup
-st.set_page_config(page_title="Python Talks Search Engine", page_icon="🐍", layout="wide")
+st.set_page_config(page_title="AI補助金サーチくん2.0", page_icon="🐍", layout="wide")
 st.title("Python Talks Search Engine")
 
 # Correct the formation of the URL
@@ -11,3 +11,6 @@ sheet_id = "1PmOf1bjCpLGm7DiF7dJsuKBne2XWkmHyo20BS4xgizw"
 sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
+
+# Use a text_input to get the keywords to filter the dataframe
+text_search = st.text_input("Search videos by title or speaker", value="")
