5e9febf317392239dc12142c560a393c92b90af2
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-17 17:41:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("AI補助金検索くん2.0")
 st.markdown("""
     <style>
     @keyframes robot {
-        0% { transform: translateY(30px); }
+        0% { transform: translateY(100px); }
         50% { transform: translateY(-100px); }
         100% { transform: translateY(-30px); }
     }
