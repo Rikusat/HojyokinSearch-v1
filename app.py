b3b5ec82d0e1f28266bd3cd7da3b3fc27dbd4059
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-18 18:51:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,6 @@ selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択して�
 # Filter the dataframe using selected 地域 and 対象事業者
 df_search = df[(df["地域"] == selected_地域) & (df["対象事業者"] == selected_対象事業者)]
 
-# Write the message on the page
-st.write(message)
-
 # Show the results and balloons
 st.write(df_search)
 st.balloons()
