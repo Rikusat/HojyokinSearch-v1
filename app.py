b739fc85cfd763ad76549d49d9fa793a9247ebe8
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-18 13:20:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ df = pd.read_csv(url, dtype=str).fillna("")
 # Get a list of unique 地域
 unique_地域 = df["地域"].unique()
 
-# Create a selectbox for 地域
-selected_地域 = st.selectbox('地域を選択してください', unique_地域)
+# Create a selectbox for 地域 in the sidebar
+selected_地域 = st.sidebar.selectbox('地域を選択してください', unique_地域)
 
 # Filter the 対象事業者 based on selected 地域
 unique_対象事業者 = df[df["地域"] == selected_地域]["対象事業者"].unique()
 
-# Create a selectbox for 対象事業者
-selected_対象事業者 = st.selectbox('対象事業者を選択してください', unique_対象事業者)
+# Create a selectbox for 対象事業者 in the sidebar
+selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択してください', unique_対象事業者)
 
 # Filter the dataframe using selected 地域 and 対象事業者
 df_search = df[(df["地域"] == selected_地域) & (df["対象事業者"] == selected_対象事業者)]
