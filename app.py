87af35771d950fe14c9cb8dfd5ff61fe8b25d72e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-18 15:27:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択して�
 
 # Filter the dataframe using selected 地域 and 対象事業者
 df_search = df[(df["地域"] == selected_地域) & (df["対象事業者"] == selected_対象事業者)]
-)
 
 # Show the results and balloons
 st.write(df_search)
