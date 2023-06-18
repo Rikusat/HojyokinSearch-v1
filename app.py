e2504c1e74a347d3fb1ff76610412fffd53fdaef
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-18 15:27:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,28 @@ selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択して�
 
 # Filter the dataframe using selected 地域 and 対象事業者
 df_search = df[(df["地域"] == selected_地域) & (df["対象事業者"] == selected_対象事業者)]
+)
 
 # Show the results and balloons
 st.write(df_search)
 st.balloons()
+
+# Get the information to ask Docsbot
+info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
+
+# Define the message input for Docsbot
+message = st.text_input("ユーザーからのメッセージ:", value=info_to_ask)
+
+if st.button("送信"):
+    # テンプレートの作成
+    info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
+    message_template = "ユーザーからのメッセージ: {}\n\n{}"
+    
+    # テンプレートにセレクトボックスの選択結果とメッセージを組み込んで送信
+    message = message_template.format(info_to_ask, message_input)
+    result = send_message_to_bot('tI6OSbQdwZIbdANCJpO9', 'LDbjERuQV2kJtkDozNIX', message)
+    st.write(result)
+
 
 
 # Show the cards
@@ -79,18 +97,3 @@ def send_message_to_bot(tI6OSbQdwZIbdANCJpO9, LDbjERuQV2kJtkDozNIX, message):
         return response.status_code, response.text  # return error information
 
 
-# Get the information to ask Docsbot
-info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
-
-# Define the message input for Docsbot
-message = st.text_input("ユーザーからのメッセージ:", value=info_to_ask)
-
-if st.button("送信"):
-    # テンプレートの作成
-    info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
-    message_template = "ユーザーからのメッセージ: {}\n\n{}"
-    
-    # テンプレートにセレクトボックスの選択結果とメッセージを組み込んで送信
-    message = message_template.format(info_to_ask, message_input)
-    result = send_message_to_bot('tI6OSbQdwZIbdANCJpO9', 'LDbjERuQV2kJtkDozNIX', message)
-    st.write(result)
