598a1b8d7ec7c1f3e9759a7ab07578d90e944be2
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-18 18:39:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -108,10 +108,20 @@ if st.sidebar.button("送信"):
     info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
     message_template = "ユーザーからのメッセージ: {}\n\n{}"
     
+# ページにテキストボックスを表示
+message_input = st.sidebar.empty()
+input_value = message_input.text_input("メッセージを入力してください")
+
+if st.sidebar.button("送信"):
+    # テンプレートの作成
+    info_to_ask = f"The selected region is {selected_地域} and the selected business is {selected_対象事業者}. There are {len(df_search)} items in the filtered list."
+    message_template = "ユーザーからのメッセージ: {}\n\n{}"
+
     # テンプレートにメッセージを組み込んで送信
-    message = message_template.format(message_input._current_value, info_to_ask)
+    message = message_template.format(input_value, info_to_ask)
     result = send_message_to_bot('tI6OSbQdwZIbdANCJpO9', 'LDbjERuQV2kJtkDozNIX', message)
     st.write(result)
+
 
 
 # Show the results and balloons
