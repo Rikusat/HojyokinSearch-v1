424b0e7c26ad092e25050cc0024b12d85caf3575
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-19 12:04:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,45 +14,36 @@ st.title("補助金検索くん🎈")
 sheet_id = "1PmOf1bjCpLGm7DiF7dJsuKBne2XWkmHyo20BS4xgizw"
 sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+# Load the data from the Google spreadsheet
 df = pd.read_csv(url, dtype=str).fillna("")
 
-# Get a list of unique 地域
-unique_地域 = df["地域"].unique()
-
-# Create a selectbox for 地域 in the sidebar
-selected_地域 = st.sidebar.selectbox('地域を選択してください', unique_地域)
-
-# Filter the 対象事業者 based on selected 地域
-unique_対象事業者 = df[df["地域"] == selected_地域]["対象事業者"].unique()
-
-# Create a selectbox for 対象事業者 in the sidebar
-selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択してください', unique_対象事業者)
-
-# Filter the dataframe using selected 地域 and 対象事業者
-df_search = df[(df["地域"] == selected_地域) & (df["対象事業者"] == selected_対象事業者)]
-
-# Show the results and balloons
-st.write(df_search)
-st.balloons()
-
-# Get the information to ask OpenAI
-info_to_ask = f"地域は {selected_地域} で、 {selected_対象事業者}が受けれる補助金を {len(df_search)} 個のリストの中から探してください"
-
 # Get user's input
-user_input = st.text_input("あなたの質問を入力してください", value=info_to_ask)
+user_input = st.text_input("あなたの質問を入力してください")
 
 if st.button("送信"):
-    # Use OpenAI API
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": user_input}
-        ]
-    )
-    # Show OpenAI's response
-    st.write(response['choices'][0]['message']['content'])
+    # Here we assume that the user's input corresponds to a 地域 in the dataframe
+    # Filter the dataframe using the user's input
+    df_search = df[df["地域"] == user_input]
 
+    # Check if the dataframe is empty
+    if df_search.empty:
+        st.write("No matching data found.")
+    else:
+        # If not, use the data to generate a message for GPT-3
+        message = f"I found {len(df_search)} matches for the 地域 '{user_input}'. Here's the first one: {df_search.iloc[0].to_dict()}"
+
+        # Use OpenAI API
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message}
+            ]
+        )
+        # Show OpenAI's response
+        st.write(response['choices'][0]['message']['content'])
+        
 # Show the cards
 N_cards_per_row = 3
 for n_row, row in df_search.reset_index().iterrows():
