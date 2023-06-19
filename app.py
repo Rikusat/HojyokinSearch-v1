0ac45dd0415be64e7bebfc76471344114f2b53f0
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-19 11:51:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ st.write(df_search)
 st.balloons()
 
 # Get the information to ask OpenAI
-info_to_ask = f"地域は {selected_地域} で、 {selected_対象事業者}が受けれる補助金を {len(df_search)} 個のリストの中から探してください。リストは次の通りです： {df_search.to_string()}"
-
+info_to_ask = f"地域は {selected_地域} で、 {selected_対象事業者}が受けれる補助金をリストの中から探してください。
 # Get user's input
 user_input = st.text_input("あなたの質問を入力してください", value=info_to_ask)
 
