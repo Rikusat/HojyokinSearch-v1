8e9403accd55ecba9c4952654df8d942746411c8
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-19 11:30:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if user_input in df.columns:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": message}
+                {"role": "user", "content": user_input}
             ]
         )
         # Show OpenAI's response
