c881049e69b77302d8ea47a430df20cda63e638e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-19 12:22:28 +0900

    Revert "Update app.py"

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.write(df_search)
 st.balloons()
 
 # Prepare the initial question
-info_to_ask = f"Please search the list of {len(df_search)} subsidies that {selected_region} {selected_covered_enterprise} is eligible for."
+info_to_ask = f"地域は {selected_地域} で、 {selected_対象事業者}が受けれる補助金を {len(df_search)} 個のリストの中から探してください"
 
 # Get user's input
 user_input = st.text_input("あなたの質問を入力してください", value=info_to_ask)
