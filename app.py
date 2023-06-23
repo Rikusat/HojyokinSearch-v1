2bbc52178ce0df16ac1fc9ce4907019bd8bb974b
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-23 13:39:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.write(df_search)
 st.balloons()
 
 # Prepare the initial question
-info_to_ask = f"地域は {selected_地域}、実施機関は {selected_実施機関}、対象事業者は {selected_対象事業者} で補助金 {len(df_search)} 個と一致するリスト"
+info_to_ask = f"地域は {selected_地域}、実施機関は {selected_実施機関}、対象事業者は {selected_対象事業者} "
 
 # Get user's input
 user_input = st.text_input("あなたの質問を入力してください", value=info_to_ask)
