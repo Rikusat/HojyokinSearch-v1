68980f655b2f0ed66551fa649fe540b2c2d649bf
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-06 19:48:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,23 +16,7 @@ sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
 
-# Show the cards
-N_cards_per_row = 3
-for n_row, row in df_search.iterrows():
-    i = n_row % N_cards_per_row
-    if i == 0:
-        st.write("---")
-        cols = st.columns(N_cards_per_row, gap="large")
-    # draw the card
-    with cols[i]:
-        st.caption(f"{row['地域'].strip()} - {row['対象事業者'].strip()} - {row['補助金名'].strip()}")
-        st.markdown(f"**申請期間: {row['申請期間'].strip()}**")
-        st.markdown(f"詳細: {row['詳細'].strip()}")
-        st.markdown(f"**[リンク]({row['リンク'].strip()})**")
-        st.markdown(f"地域: {row['地域'].strip()}")
-        st.markdown(f"実施機関: {row['実施機関'].strip()}")
-        st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
-        st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
+
 
 
 # 対象事業者の各文字列を取得して一意の値を生成
@@ -54,3 +38,21 @@ df_search = df[df["対象事業者"].apply(lambda x: all(opt in x.split("／") f
 # Show the results and balloons
 st.write(df_search)
 st.balloons()
+
+# Show the cards
+N_cards_per_row = 3
+for n_row, row in df_search.iterrows():
+    i = n_row % N_cards_per_row
+    if i == 0:
+        st.write("---")
+        cols = st.columns(N_cards_per_row, gap="large")
+    # draw the card
+    with cols[i]:
+        st.caption(f"{row['地域'].strip()} - {row['対象事業者'].strip()} - {row['補助金名'].strip()}")
+        st.markdown(f"**申請期間: {row['申請期間'].strip()}**")
+        st.markdown(f"詳細: {row['詳細'].strip()}")
+        st.markdown(f"**[リンク]({row['リンク'].strip()})**")
+        st.markdown(f"地域: {row['地域'].strip()}")
+        st.markdown(f"実施機関: {row['実施機関'].strip()}")
+        st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
+        st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
