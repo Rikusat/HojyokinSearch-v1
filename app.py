5c107a961730b8c64bd1c8fe20079eb3b52aaa98
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-06 20:09:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ st.balloons()
 
 # Show the cards
 N_cards_per_row = 3
+cols = st.columns(N_cards_per_row, gap="large")
 for n_row, row in df_search.iterrows():
     i = n_row % N_cards_per_row
     if i == 0:
         st.write("---")
-        cols = st.columns(N_cards_per_row, gap="large")
     # draw the card
     with cols[i]:
         st.caption(f"{row['補助金名'].strip()}")
@@ -56,3 +56,4 @@ for n_row, row in df_search.iterrows():
         st.markdown(f"実施機関: {row['実施機関'].strip()}")
         st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
         st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
+
