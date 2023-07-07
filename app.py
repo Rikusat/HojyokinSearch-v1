dee519e1ce01ba72d5d10552780408a3b52aeac4
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-08 01:46:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,22 @@ sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
 
+
 # Get a list of unique 地域
-unique_地域 = df["地域"].unique()
+unique_実施機関 = df["実施機関"].unique()
 
-# Create a selectbox for 地域
-selected_地域 = st.selectbox('地域を選択してください', unique_地域)
+# Create a selectbox for 実施機関 in the sidebar
+selected_実施機関 = st.sidebar.selectbox('実施機関を選択してください', unique_実施機関)
 
-# Filter the dataframe based on selected 地域
-df_filtered = df[df["地域"] == selected_地域]
+# Filter the 対象事業者 based on selected 地域
+unique_対象事業者 = df[df["実施機関"] == selected_実施機関]["対象事業者"].unique()
+
+# Create a selectbox for 対象事業者 in the sidebar
+selected_対象事業者 = st.sidebar.selectbox('対象事業者を選択してください', unique_対象事業者)
+
+# Filter the dataframe using selected 地域 and 対象事業者
+df_search = df[(df["実施機関"] == selected_実施機関) & (df["対象事業者"] == selected_対象事業者)]
+
 
 # 対象事業者の各文字列を取得して一意の値を生成
 filter_options = set()
