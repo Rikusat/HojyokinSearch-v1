04f4adbc22ff73970a7e82e73d3f5930648efe1a
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-08 17:38:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,28 @@ sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
 
+# Function to filter data based on selected 地域 and selected_options
+def filter_data(selected_地域, selected_options):
+    df = pd.read_csv(url, dtype=str).fillna("")
+    df_filtered = df[(df["地域"] == selected_地域) & (df["対象事業者"].str.contains("|".join(selected_options)))]
+    return df_filtered
+
 # Get a list of unique 地域
+df = pd.read_csv(url, dtype=str).fillna("")
 unique_地域 = df["地域"].unique()
 
 # Create a selectbox for 地域
 selected_地域 = st.selectbox('地域を選択してください', unique_地域)
 
-# Filter the dataframe based on selected 地域
-df_filtered = df[df["地域"] == selected_地域]
-
 # 対象事業者の各文字列を取得して一意の値を生成
 filter_options = set()
-for item in df_filtered["対象事業者"]:
+for item in df[df["地域"] == selected_地域]["対象事業者"]:
     options = item.split("／")
     filter_options.update(options)
 
 # フィルタリング用の選択ボックスを作成
 cols = 4  # 1行に表示するチェックボックスの数
 selected_options = []
-checkboxes = []
 for i, option in enumerate(filter_options):
     if i % cols == 0:
         col = st.beta_columns(cols)
@@ -45,34 +48,32 @@ for i, option in enumerate(filter_options):
     selected = col[i % cols].checkbox(option, key=checkbox_key, value=(option in selected_options))
     if selected:
         selected_options.append(option)
-    checkboxes.append(selected)
 
 # 選択されたオプションを表示
 selected_options = list(set(selected_options))  # 重複を削除
 st.write("選択されたオプション:", selected_options)
 
 # フィルタリング
-df_search = df_filtered[df_filtered["対象事業者"].str.contains("|".join(selected_options))]
-
-# Show the results and balloons
-st.write(df_search)
-st.balloons()
+df_search = filter_data(selected_地域, selected_options)
 
 # Show the cards
-N_cards_per_row = 3
-cols = st.columns(N_cards_per_row, gap="large")
-for n_row, row in df_search.iterrows():
-    i = n_row % N_cards_per_row
-    if i == 0:
-        st.write("---")
-    # draw the card
-    with cols[i]:
-        st.markdown(f"**{row['補助金名'].strip()}**")
-        st.caption(f"{row['申請期間'].strip()}")
-        st.markdown(f"{row['詳細'].strip()}")
-        st.markdown(f"{row['上限金額・助成額'].strip()}")
-        st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
-        st.markdown(f"地域: {row['地域'].strip()}")
-        st.markdown(f"実施機関: {row['実施機関'].strip()}")
-        st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
-        st.markdown(f"**[掲載元]({row['掲載元'].strip()})**")
+if df_search.empty:
+    st.write("No matching data found.")
+else:
+    N_cards_per_row = 3
+    cols = st.columns(N_cards_per_row, gap="large")
+    for n_row, row in df_search.iterrows():
+        i = n_row % N_cards_per_row
+        if i == 0:
+            st.write("---")
+        # draw the card
+        with cols[i]:
+            st.markdown(f"**{row['補助金名'].strip()}**")
+            st.caption(f"{row['申請期間'].strip()}")
+            st.markdown(f"{row['詳細'].strip()}")
+            st.markdown(f"{row['上限金額・助成額'].strip()}")
+            st.markdown(f"**[掲載元]({row['掲載元'].strip()})**")
+            st.markdown(f"地域: {row['地域'].strip()}")
+            st.markdown(f"実施機関: {row['実施機関'].strip()}")
+            st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
+            st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
