4294112c070b12610cfa77d7bf2e522cd82d2f6e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-08 18:20:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import pandas as pd
 import streamlit as st
 import openai
 
-import pandas as pd
-import streamlit as st
-import openai
-
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
