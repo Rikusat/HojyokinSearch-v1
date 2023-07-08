c3e191d240372fe48e8679c6e4ff29ebcd7c789e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-08 20:54:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ for n_row, row in df_search.iterrows():
         st.markdown(f"地域: {row['地域'].strip()}")
         st.markdown(f"実施機関: {row['実施機関'].strip()}")
         st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
-        st.markdown(f"**[掲載元]({row['掲載元'].strip()})**")
         st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
+        st.markdown(f"**[掲載元]({row['掲載元'].strip()})**")
