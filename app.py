d210ca4322b5156deac33a6135789e211938a551
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-10 03:18:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,15 +31,8 @@ col1, col2 = st.beta_columns(2)
 selected_地域 = col1.selectbox('地域を選択', unique_地域, index=0)
 selected_options = col2.multiselect("当てはまる項目を選択 : 複数可", list(filter_options))
 
-# 対象事業者の各文字列を取得して一意の値を生成
-filter_options = set()
-for item in df[df["地域"] == selected_地域]["対象事業者"]:
-    options = item.split("／")
-    filter_options.update(options)
-    
 # フィルタリング
 df_search = filter_data(selected_地域, selected_options)
-
 # Prepare the initial question
 info_to_ask = f"地域:{selected_地域} 対象事業者:{', '.join(selected_options)} "
 
