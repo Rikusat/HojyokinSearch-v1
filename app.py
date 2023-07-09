d070002a5b1c295598f0c4bce0b285d5d461e2db
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-07-10 04:18:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,3 +87,4 @@ for n_row, row in df_search.iterrows():
         st.markdown(f"対象事業者: {row['対象事業者'].strip()}")
         st.markdown(f"公式公募ページ: {row['公式公募ページ'].strip()}")
         st.markdown(f"**[掲載元]({row['掲載元'].strip()})**")
+        st.markdown("---")
