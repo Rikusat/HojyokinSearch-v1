36c0a75bb9274a5561f8fee25f8fa0c156a82507
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-12-10 01:17:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,16 @@ def get_google_sheet_data(sheet_url):
     sheet_id = sheet_url.split("/")[5]  # Google Sheets の URL からシートのIDを取得
     sheet = gc.open_by_key(sheet_id).sheet1  # シート1を読み込み（シート名が異なる場合は変更してください）
 
+    replacements = {}
     data = sheet.get_all_values()
-    headers = data[0]
-    values = data[1:]
 
-    return headers, values
+    for row in data:
+        if len(row) >= 2:
+            old_text = row[0]
+            new_text = row[1]
+            replacements[old_text] = new_text
+
+    return replacements
 
 def main():
     st.title('Word書類の文字列置換アプリ')
@@ -36,35 +41,22 @@ def main():
     # Google Sheets の URL をアップロード
     sheet_url = st.text_input("Google Sheets の共有可能なリンクを入力してください")
 
-    if sheet_url:
-        headers, values = get_google_sheet_data(sheet_url)
+    # Wordファイルのアップロード
+    st.sidebar.header('Wordファイルをアップロード')
+    word_file = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'])
 
-        # Google Sheets の内容をテーブル形式で表示
-        st.subheader("Google Sheets の内容:")
-        df = pd.DataFrame(values, columns=headers)
-        editable_table = st.table(df)
+    if sheet_url and word_file:
+        # Google Sheets から置換情報を取得
+        replacements = get_google_sheet_data(sheet_url)
 
-        if st.button('セルの変更を適用'):
-            new_values = editable_table.value
+        # Wordファイルの置換
+        replace_text_in_word(word_file.name, "output.docx", replacements)
 
-            # 置換情報の作成
-            replacements = {}
-            for index, row in new_values.iterrows():
-                replacements[row[headers[0]]] = row[headers[1]]
-
-            # Wordファイルのアップロード
-            st.sidebar.header('Wordファイルをアップロード')
-            word_file = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'])
-
-            if word_file:
-                # Wordファイルの置換
-                replace_text_in_word(word_file.name, "output.docx", replacements)
-
-                # ダウンロードリンクの作成
-                with open("output.docx", "rb") as file:
-                    file_contents = file.read()
-                    st.sidebar.markdown(get_binary_file_downloader_html(file_contents, file_name="output.docx"), unsafe_allow_html=True)
-                    st.success("置換が完了しました。")
+        # ダウンロードリンクの作成
+        with open("output.docx", "rb") as file:
+            file_contents = file.read()
+            st.sidebar.markdown(get_binary_file_downloader_html(file_contents, file_name="output.docx"), unsafe_allow_html=True)
+            st.success("置換が完了しました。")
 
 def get_binary_file_downloader_html(bin_file, file_name, button_text="Click here to download"):
     import base64
