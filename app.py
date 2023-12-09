a87d8e6749d0f2ad95d0ca6ef022e45aa1a5279e
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-12-10 01:35:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ import pandas as pd
 import io
 import os
 
-# ... 以前のコード ...
+def replace_text_in_word(input_word_file, output_word_file, replacements):
+    with open(input_word_file, 'rb') as file:
+        doc_bytes = io.BytesIO(file.read())
+
+    doc_text = doc_bytes.getvalue().decode("utf-8")
+
+    for old_text, new_text in replacements.items():
+        doc_text = doc_text.replace(old_text, new_text)
+
+    with open(output_word_file, 'wb') as file:
+        file.write(doc_text.encode("utf-8"))
+
+def display_excel_table(excel_file):
+    df = pd.read_excel(excel_file)
+    st.subheader("Excelファイルの内容:")
+    st.write(df)
 
 def main():
     st.title('Word書類の文字列置換アプリ')
@@ -37,12 +52,32 @@ def main():
         wb = load_workbook(excel_path)
         ws = wb.active
 
-        # ... 以前の置換処理 ...
+        replacements = {}
+        max_col = ws.max_column
+        max_row = ws.max_row
+
+        for row in range(1, max_row + 1):
+            for col in range(1, max_col + 1):
+                old_text = ws.cell(row=row, column=col).value
+                new_text = ws.cell(row=row, column=col + 1).value
+
+                if old_text is not None and new_text is not None:
+                    replacements[old_text] = new_text
 
         # Wordファイルの置換
         replace_text_in_word(word_path, "output.docx", replacements)
 
-        # ... 以前のダウンロードリンクの作成 ...
+        # ダウンロードリンクの作成
+        with open("output.docx", "rb") as file:
+            file_contents = file.read()
+            st.sidebar.markdown(get_binary_file_downloader_html(file_contents, file_name="output.docx"), unsafe_allow_html=True)
+            st.success("置換が完了しました。")
+
+def get_binary_file_downloader_html(bin_file, file_name, button_text="Click here to download"):
+    import base64
+    bin_str = base64.b64encode(bin_file).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_name}">{button_text}</a>'
+    return href
 
 if __name__ == '__main__':
     main()
