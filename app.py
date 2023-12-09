72c7e089879d9e2de409d0032216fa9014700f30
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-12-10 00:44:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,47 @@
 import streamlit as st
+from openpyxl import load_workbook
 from docx import Document
 
+def replace_text_in_word(input_word_file, output_word_file, old_text, new_text):
+    doc = Document(input_word_file)
+    for paragraph in doc.paragraphs:
+        if old_text in paragraph.text:
+            paragraph.text = paragraph.text.replace(old_text, new_text)
+    doc.save(output_word_file)
+
 def main():
-    st.title('Wordファイルのプレビュー')
+    st.title('Word書類の文字列置換アプリ')
+
+    # Excelファイルのアップロード
+    st.sidebar.header('Excelファイルをアップロード')
+    excel_file = st.sidebar.file_uploader("Excelファイルを選択してください", type=['xlsx'])
 
     # Wordファイルのアップロード
     st.sidebar.header('Wordファイルをアップロード')
-    uploaded_file = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'])
+    word_file = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'])
 
-    if uploaded_file is not None:
-        # アップロードされたファイルの情報を表示
-        file_details = {
-            "FileName": uploaded_file.name,
-            "FileType": uploaded_file.type,
-            "FileSize": uploaded_file.size
-        }
-        st.sidebar.write(file_details)
+    if excel_file and word_file:
+        # Excelファイルから文字列を取得
+        wb = load_workbook(excel_file)
+        ws = wb.active
+        old_text = ws['B1'].value
+        new_text = ws['B2'].value
 
-        # 一部のテキスト内容を表示（例として最初の段落のテキストを表示）
-        doc = Document(uploaded_file)
-        first_paragraph = doc.paragraphs[0].text if len(doc.paragraphs) > 0 else "No text found"
-        st.write("First Paragraph:", first_paragraph)
+        # Wordファイルの置換
+        replace_text_in_word(word_file, "output.docx", old_text, new_text)
+
+        # ダウンロードリンクの作成
+        with open("output.docx", "rb") as file:
+            file_contents = file.read()
+            st.sidebar.markdown(get_binary_file_downloader_html(file_contents, file_name="output.docx"), unsafe_allow_html=True)
+            st.success("置換が完了しました。")
+
+def get_binary_file_downloader_html(bin_file, file_name, button_text="Click here to download"):
+    import base64
+    bin_str = base64.b64encode(bin_file).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_name}">{button_text}</a>'
+    return href
 
 if __name__ == '__main__':
     main()
+
