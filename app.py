3621b0232032e82ce99f8041f52c680d782e3d90
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-12-10 00:31:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import streamlit as st
-import docx
 
-def replace_text_in_word(input_word_file, output_word_file, old_text, new_text):
-    doc = docx.Document(input_word_file)
-    for paragraph in doc.paragraphs:
-        if old_text in paragraph.text:
-            paragraph.text = paragraph.text.replace(old_text, new_text)
-    doc.save(output_word_file)
+def replace_text_in_word(file_path, old_text, new_text):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+        new_text = text.replace(old_text, new_text)
+    with open('output.txt', 'w', encoding='utf-8') as output_file:
+        output_file.write(new_text)
 
 def main():
     st.title('Word書類の文字列置換アプリ')
 
-    # Wordファイルのアップロード
-    st.sidebar.header('Wordファイルをアップロード')
-    uploaded_file = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'])
+    # テキストファイルのアップロード
+    st.sidebar.header('テキストファイルをアップロード')
+    uploaded_file = st.sidebar.file_uploader("テキストファイルを選択してください", type=['txt'])
 
     if uploaded_file is not None:
         # アップロードされたファイルを一時保存
-        with open("temp.docx", "wb") as file:
+        with open("temp.txt", "wb") as file:
             file.write(uploaded_file.getvalue())
 
         # テキスト置換用の入力
@@ -25,12 +24,11 @@ def main():
         new_text = st.sidebar.text_input("置換後のテキスト")
 
         if old_text and new_text:
-            replace_text_in_word("temp.docx", "output.docx", old_text, new_text)
+            replace_text_in_word("temp.txt", old_text, new_text)
 
             # ダウンロードリンクの作成
-            st.sidebar.markdown("[**ダウンロード新しいWordファイル**](./output.docx)", unsafe_allow_html=True)
+            st.sidebar.markdown("[**ダウンロード新しいテキストファイル**](./output.txt)", unsafe_allow_html=True)
             st.success("置換が完了しました。")
 
 if __name__ == '__main__':
     main()
-
