8671a6c461de056c6ae764f152bbab8c0f4f4ffc
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-12-10 01:55:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,61 @@
 import streamlit as st
 import pandas as pd
+from docx import Document
 
-def replace_text(text, replacements):
+def replace_text_in_word(input_word_file, output_word_file, replacements):
+    doc = Document(input_word_file)
+
     for old_text, new_text in replacements.items():
-        text = text.replace(old_text, new_text)
-    return text
+        for paragraph in doc.paragraphs:
+            if old_text in paragraph.text:
+                paragraph.text = paragraph.text.replace(old_text, new_text)
+
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    if old_text in cell.text:
+                        cell.text = cell.text.replace(old_text, new_text)
+
+    doc.save(output_word_file)
 
 def main():
-    st.title('テキスト置換アプリ')
+    st.title('Wordファイルの文字列置換アプリ')
 
-    # テキスト入力
-    st.header('テキストを入力してください')
-    input_text = st.text_area('テキストを入力してください')
+    # Excelファイルのアップロード
+    st.sidebar.header('Excelファイルをアップロード')
+    excel_file = st.sidebar.file_uploader("Excelファイルを選択してください", type=['csv'])
 
-    # CSVファイルのアップロード
-    st.header('CSVファイルをアップロード')
-    csv_file = st.file_uploader('CSVファイルを選択してください', type=['csv'])
+    # Wordファイルのアップロード（複数ファイル）
+    st.sidebar.header('Wordファイルを複数選択してください')
+    word_files = st.sidebar.file_uploader("Wordファイルを選択してください", type=['docx'], accept_multiple_files=True)
 
-    if input_text and csv_file:
+    if excel_file and word_files:
         # CSVファイルから置換情報を取得
-        df = pd.read_csv(csv_file)
+        df = pd.read_csv(excel_file)
         replacements = dict(zip(df['Old Text'], df['New Text']))
 
-        # テキストの置換
-        result_text = replace_text(input_text, replacements)
+        # Wordファイルごとに置換処理を実行
+        for word_file in word_files:
+            # Wordファイルの一時保存
+            word_path = f"./temp_word_{word_file.name}"
+            with open(word_path, "wb") as temp_word:
+                temp_word.write(word_file.read())
 
-        # 置換結果を表示
-        st.header('置換結果')
-        st.text_area('置換されたテキスト', value=result_text, height=200)
+            # Wordファイルの置換処理
+            output_word_path = f"./output_{word_file.name}"
+            replace_text_in_word(word_path, output_word_path, replacements)
 
-        # 置換結果をダウンロード可能なCSVファイルとして提供
-        result_csv = df.assign(Result_Text=result_text)
-        st.markdown(get_csv_download_link(result_csv), unsafe_allow_html=True)
+            # ダウンロードリンクの作成
+            with open(output_word_path, "rb") as file:
+                file_contents = file.read()
+                st.sidebar.markdown(get_binary_file_downloader_html(file_contents, file_name=output_word_path), unsafe_allow_html=True)
 
-def get_csv_download_link(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="replaced_text.csv">Download CSV File</a>'
+        st.sidebar.success("置換が完了しました。")
+
+def get_binary_file_downloader_html(bin_file, file_name, button_text="Click here to download"):
+    import base64
+    bin_str = base64.b64encode(bin_file).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_name}">{button_text}</a>'
     return href
 
 if __name__ == '__main__':
