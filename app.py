e42d833da1dceb055cd47901ececafcbd78e5d83
Rikusat <91641392+Rikusat@users.noreply.github.com>
2024-01-06 00:43:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.title("関東圏：補助金検索くん🎈")
 sheet_id = "1s-LHhUIa-SgYJFHggP94LyG-KXqaNr_Xx7SPROtTaSI"
 sheet_name = "charlas"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-        
-        
+df = pd.read_csv(url, dtype=str)
+
 # Show the cards
 N_cards_per_row = 3
 cols = st.columns(N_cards_per_row, gap="large")
